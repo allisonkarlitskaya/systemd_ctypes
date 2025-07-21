allisonkarlitskaya/systemd_ctypes

commit cd4c6cfc498075548e559fd5dee208fde5fef9bd

event: Use event loop factory with Python ≥ 3.12
`set_event_loop_policy()` is deprecated in Python 3.14. 3.12 introduced
a `loop_factory` argument to `run_async()` as replacement.

Fixes #72

src/systemd_ctypes/event.py
@@ -100,14 +100,18 @@ class Selector(selectors.DefaultSelector):
         return [(key, events) for (key, events) in ready if key != self.key]
 
 
+def selector_event_loop_factory() -> asyncio.AbstractEventLoop:
+    """Factory function to create an asyncio event loop using Selector."""
+    return asyncio.SelectorEventLoop(Selector())
+
+
+# deprecated in Python 3.12
 class EventLoopPolicy(asyncio.DefaultEventLoopPolicy):
     def new_event_loop(self) -> asyncio.AbstractEventLoop:
-        return asyncio.SelectorEventLoop(Selector())
+        return selector_event_loop_factory()
 
 
 def run_async(main: Coroutine[None, None, None], debug: Optional[bool] = None) -> None:
-    asyncio.set_event_loop_policy(EventLoopPolicy())
-
     polyfill = sys.version_info < (3, 7, 0) and not hasattr(asyncio, 'run')
     if polyfill:
         # Polyfills for Python 3.6:
@@ -132,7 +136,11 @@ def run_async(main: Coroutine[None, None, None], debug: Optional[bool] = None) -
 
         asyncio._systemd_ctypes_polyfills = True  # type: ignore[attr-defined]
 
-    asyncio.run(main, debug=debug)
+    if sys.version_info >= (3, 12, 0):
+        asyncio.run(main, debug=debug, loop_factory=selector_event_loop_factory)
+    else:
+        asyncio.set_event_loop_policy(EventLoopPolicy())
+        asyncio.run(main, debug=debug)
 
     if polyfill:
         del asyncio.create_task, asyncio.get_running_loop, asyncio.run

commit a3b0933cc3317dab4bfc6c3147596c5e9ef2f282

event: Re-fix run_async for RHEL 8
Commit cd4c6cfc498 broke Python 3.6 on RHEL 8 -- we need to set the
event loop policy before the polyfills.

src/systemd_ctypes/event.py
@@ -114,6 +114,11 @@ class EventLoopPolicy(asyncio.DefaultEventLoopPolicy):
 
 def run_async(main: Coroutine[None, None, None], debug: Optional[bool] = None) -> None:
     polyfill = sys.version_info < (3, 7, 0) and not hasattr(asyncio, 'run')
+    loop_factory = sys.version_info >= (3, 12, 0)
+
+    if not loop_factory:
+        asyncio.set_event_loop_policy(EventLoopPolicy())
+
     if polyfill:
         # Polyfills for Python 3.6:
         loop = asyncio.get_event_loop()
@@ -137,10 +142,9 @@ def run_async(main: Coroutine[None, None, None], debug: Optional[bool] = None) -
 
         asyncio._systemd_ctypes_polyfills = True  # type: ignore[attr-defined]
 
-    if sys.version_info >= (3, 12, 0):
+    if loop_factory:
         asyncio.run(main, debug=debug, loop_factory=selector_event_loop_factory)
     else:
-        asyncio.set_event_loop_policy(EventLoopPolicy())
         asyncio.run(main, debug=debug)
 
     if polyfill: