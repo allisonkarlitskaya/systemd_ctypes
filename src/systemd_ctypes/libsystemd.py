15513bc19a2d3e547c4ac981f8dd0c6936fcadd0
Allison Karlitskaya <allison.karlitskaya@redhat.com>
2026-09-17 12:16:30 +0200

    libsystemd: ignore RUF036, keep Union[None, Errno]
    The way that this is written this way is:
     - this code needs to run on Python 3.6 where `|` wasn't yet a thing
    
     - this is about systemd-style integer return codes where the "None" is
       the normal case here (0) and Errno is the exceptional one
       (negative-errno value) which actually results in an exception being
       raised.  In other places we write Union[int, Errno] for positive
       integer successful return values.  That's a bit different than almost
       every case that this rule was probably intended to cover, so let's
       just disable it.

--- src/systemd_ctypes/libsystemd.py
+++ src/systemd_ctypes/libsystemd.py
@@ -15,6 +15,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+# ruff: noqa: RUF036
+# None-first Union ordering is intentional: None is the success case, Errno is the exceptional one
+
 import ctypes
 import os
 import sys
